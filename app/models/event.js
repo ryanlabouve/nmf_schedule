4ddb692938d28e19c1c26a59a8049995a3530138
Jeremy Green <jeremy@octolabs.com>
2016-04-14 23:30:07 -0500

    Generate an event model

---
+++ app/models/event.js
@@ -0,0 +1,9 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+
+export default Model.extend({
+  bandName: attr(),
+  day: attr(),
+  time: attr(),
+  stageName: attr()
+});
