f3b87b2080cb1324993c89c99e27b989158c06d2
Jeremy Green <jeremy@octolabs.com>
2016-04-17 12:17:26 -0500

    Polishing

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -8,5 +8,5 @@ const {
 
 export default Controller.extend({
   routing: service('-routing'),
-  isHome: computed.equal('routing.currentPath', 'day.index')
+  showArrow: computed.equal('routing.currentPath', 'day.event')
 });
