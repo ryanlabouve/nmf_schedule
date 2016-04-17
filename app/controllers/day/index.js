998fece5858afda880a0b657be3e53b6f7e3d727
Jeremy Green <jeremy@octolabs.com>
2016-04-17 00:11:27 -0500

    wip

--- app/controllers/day/index.js
+++ app/controllers/day/index.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   sortProps: ['sortableTime','stageName'],
-  sortedEvents: Ember.computed.sort('model.events', 'sortProps')
+  sortedEvents: Ember.computed.sort('model.events', 'sortProps'),
+  actions: {
+    chooseTalk(event) {
+      let talkId = $(event.target).closest('li').data('talk-id');
+      if (talkId && event.target.nodeName !== 'BUTTON') {
+        this.transitionToRoute('day.talk', this.get('model.id'), talkId);
+      }else{
+        console.log('skipping!');
+      }
+    }
+  }
 });
 
